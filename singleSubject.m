f681be68a6aa598d833215ba99729da58ebfde6b
jordandekraker <jdekrake@uwo.ca>
2020-06-18 09:14:19 -0400

    Revert "Singularity version"

--- singleSubject.m
+++ singleSubject.m
@@ -1,11 +1,5 @@
 function singleSubject(inimg,outdir,inlbl,space)
 
-autotop_dir = getenv('AUTOTOP_DIR');
-if isempty(autotop_dir)
-    disp('ERROR: you must set the AUTOTOP_DIR environment variable before running');
-    quit(1);
-end
-
 addpath(genpath('tools'));
 mkdir(outdir);
 outdir = [outdir '/']; % make sure this is a directory
