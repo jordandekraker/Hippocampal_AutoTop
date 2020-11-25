c3f82f63c788e90e79417ef6c45ecc3b48e3b951
akhanf <alik@robarts.ca>
2020-11-25 16:18:35 -0500

    minor update to create_subj_isosurf, still needs work

--- tools/create_subj_isosurf.m
+++ tools/create_subj_isosurf.m
@@ -1,19 +1,34 @@
-function create_subj_isosurf (in_coords_io_nii, out_surf_gii, out_surf_smoothed_gii)
+function create_subj_isosurf (in_coords_io_nii, out_prefix)
 
+% this is still a work in progress, needs hole-filling, and then another function to resample to a different mesh in the unfolded space
+%   (i.e. could be similar to -surface-resample  or -metric-resample;  <in_surf> <curr_flat_mesh> <new_flat_mesh> <out_surf>)
 
+%-- example args
 %in_coords_io_nii = 'coords-IO.nii.gz';
+%out_prefix = './';
+%--
 
 io_img = niftiread(in_coords_io_nii);
 img_info = niftiinfo(in_coords_io_nii);
 
 affine = img_info.Transform.T';
 
+%smoothing params
+VoxSize = 0.3;
+DisplTol = 0.001*VoxSize; %default 0.01*VoxSize
+IterTol = 100; %default is 100
+Freedom = 1; %default 2, unrestricts normal and tangential smoothing -- 1 just unrestricts tangential
+
 
 %set non gm to nan
 io_img(io_img==0) = nan;
 
+io_threshold = 0.5;
+surf_name = 'midthickness';
+
+
 %generate isosurf
-[faces, vert_iso ] = isosurface(io_img,0.5);
+[faces, vert_iso ] = isosurface(io_img,io_threshold);
 
 %isosurface uses meshgrid, so need to swap X and Y
 vertices = zeros(size(vert_iso));
@@ -35,7 +50,7 @@ G = graph([faces(:,1); faces(:,1); faces(:,2)],[faces(:,2); faces(:,3); faces(:,
 A = adjacency(G);
 [numcomponents, C] = graphconncomp(A);
 
-%first component is the largest   
+%first component is the largest
 %keep only the vertices in the largest component:
 [vertices_pruned, faces_pruned] = remove_vertices(vertices, faces, find(C>1));
 
@@ -45,16 +60,12 @@ A = adjacency(G);
 [numcomponents, C] = graphconncomp(A);
 %assert numcomponents == 1
 
-%----  smoooth the surface -- this was here when we had voxelated surfaces,
-%not really necessary anymore.. 
+%----  smoooth the surface --
+%not really necessary anymore..
 
-VoxSize = 0.3;
-DisplTol = 0.01*VoxSize;
-IterTol = 100; %default is 100
-Freedom = 2; %default 2, unrestricts normal and tangential smoothing -- 1 just unrestricts tangential
 vertices_smoothed = SurfaceSmooth(vertices_pruned, faces_pruned, VoxSize,DisplTol, IterTol, Freedom);
 
-%---- to do: 
+%---- to do:
 
 % identify and fill holes (topological correction)
 % clean up the boundary (fix the jagged edges)
@@ -64,27 +75,29 @@ g = gifti;
 g.mat = eye(4,4);
 g.faces = int32(faces);
 g.vertices = single(vertices);
-save(g,'midthickness.iso.surf.gii','Base64Binary');
+save(g,sprintf('%s%s.iso.surf.gii',out_prefix,surf_name),'Base64Binary');
 
 g_pruned = g;
 g_pruned.faces = int32(faces_pruned);
 g_pruned.vertices = single(vertices_pruned);
-save(g_pruned,'midthickness.isopruned.surf.gii','Base64Binary');
+save(g_pruned,sprintf('%s%s.isopruned.surf.gii',out_prefix,surf_name),'Base64Binary');
 
 g_smooth = g_pruned;
 g_smooth.vertices = single(vertices_smoothed);
-save(g_smooth,'midthickness.isosmoothed.surf.gii','Base64Binary');
+save(g_smooth,sprintf('%s%s.isosmoothed.surf.gii',out_prefix,surf_name),'Base64Binary');
 
 %plot them
 figure;
 plot(g);
+title(sprintf('%s.iso.surf.gii',surf_name));
 axis equal;
 figure;
 plot(g_pruned);
+title(sprintf('%s.isopruned.surf.gii',surf_name));
 axis equal;
 figure;
 plot(g_smooth);
+title(sprintf('%s.isosmoothed.surf.gii',surf_name));
 axis equal;
 
 end
-
