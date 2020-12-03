7af284a8f3111482434cc6c5b08d7db602ac4c1d
akhanf <alik@robarts.ca>
2020-12-02 21:10:13 -0500

    updated template surf creation, and file naming

--- tools/warps_gifti/create_template_unfold_gifti.m
+++ tools/warps_gifti/create_template_unfold_gifti.m
@@ -7,17 +7,7 @@ n_steps_unfold = [256 128 16];
 
 %get path to reference nifti relative to this script (../misc)
 [path,name,ext] = fileparts(mfilename('fullpath'));
-unfold_ref_nii = [path '/../misc/unfold_ref_256x128x16.nii.gz'];
-
-
-% This function will generate the following files in the
-% template unfolded space:
-%  midthickness_254x126.unfolded.surf.gii
-%  inner_254x126.unfolded.surf.gii
-%  outer_254x126.unfolded.surf.gii
-%
-% Then, you can use wb_command with a subject's warp to transform to native
-% wb_command  -surface-apply-warpfield  midthickness_254x126.unfolded.surf.gii Warp_unfold2native.nii midthickness_254x126.native.surf.gii 
+unfold_ref_nii = [path '/../../misc/unfold_ref_256x128x16.nii.gz'];
 
 
 
@@ -27,21 +17,40 @@ unfold_info.ImageSize = [unfold_info.ImageSize,3];
 unfold_info.PixelDimensions = [unfold_info.PixelDimensions,1];
 affine_unfold = unfold_info.Transform.T';
 
+% we want the vertices to lie in the *centre* of a voxel;
+% e.g. for voxel 0,0,0; and if voxel spacing is 1mm
+%   then we want the vertex placed at 0.5,0.5,0.5;  so instead of index i,
+%   we use index i+0.5;  from matlab index J, we want J-0.5
 
 
+samplingu=0.5:1:(n_steps_unfold(1)-0.5);
+samplingv=0.5:1:(n_steps_unfold(2)-0.5);  
+samplingw=0.5:1:(n_steps_unfold(3)-1);
+n_steps_unfold(3) = n_steps_unfold(3)-1;
 
-%if transforming with wb_command, will complain if the points are not
-%completed enclosed by the warp volume -- e.g. has an issue with the most
-%extreme vertices.. to get around that, we should define the warp volume
-%larger (i.e. extend laplace's field) -- but for now, will just create a
-%flat mesh with the border vertices removed.. with a large discretization
-%this is not a big deal..
+%and the corresponding meshgrid
+[gv,gu,gw]=meshgrid(samplingv,samplingu,samplingw);
 
+%convert inds from 0-(N-1) to phys inds in unfolded space, byapplying
+%affine (first cat into vec)
+unfold_coords_mat = cat(4,gu,gv,gw);
+unfold_coords_mat = reshape(unfold_coords_mat,[prod(size(unfold_coords_mat,1,2,3)),3]);
+unfold_coords_mat = [unfold_coords_mat,ones(size(unfold_coords_mat,1),1)]';
+unfold_coords_phys = affine_unfold*unfold_coords_mat;
+unfold_coords_phys = unfold_coords_phys(1:3,:)';
+
+%reshape back to an image
+unfold_coords_phys_img = reshape(unfold_coords_phys,[size(gu,1,2,3),3]);
+
+% --- for backwards compatibility with earlier surfaces, we use 254x126
+% vertices instead of 256x128.. not a big deal I think..
+
+%n_steps_unfold_crop = [n_steps_unfold(1)-2,n_steps_unfold(2)-2,1];
 n_steps_unfold_crop = [n_steps_unfold(1)-2,n_steps_unfold(2)-2,1];
 
 %we create three 2-d surfaces:
 % the inner and outer 
-io_inds = [3,ceil(n_steps_unfold(3)/2),n_steps_unfold(3)-2];
+io_inds = [1,ceil(n_steps_unfold(3)/2),n_steps_unfold(3)];
 surf_names = {'inner','midthickness','outer'};
 
 
@@ -56,44 +65,6 @@ F(:,:,1,2) = nan;
 F(isnan(F)) = [];
 F=reshape(F,[3,(n_steps_unfold_crop(1)-1)*(n_steps_unfold_crop(2)-1)*2])';
 
-%build sampling vectors in normalized unfolded space (0-1)
-samplingu=0:1/((n_steps_unfold(1)-1)):1;  %here N is setting the number of steps
-samplingv=0:1/(n_steps_unfold(2)-1):1;
-samplingw=0:1/(n_steps_unfold(3)-1):1;
-
-%and the corresponding meshgrid
-[gv,gu,gw]=meshgrid(samplingv,samplingu,samplingw);
-
-%first get coords in the space of the unfold nii (which is from 0 to N-1),
-%not 0-1
-gu_ = gu.*(n_steps_unfold(1)-1);  % here, N is setting the physical dimensions
-gv_ = gv.*(n_steps_unfold(2)-1);
-gw_ = gw.*(n_steps_unfold(3)-1);
-
-
-%create a nifti from scratch for unfold space - as want this to be same for
-%all subjects, regardless of native space nifti
-unfold_ref_nii = [tempname '.nii'];
-unfold_ref = zeros([n_steps_unfold,3]);
-
-niftiwrite(single(unfold_ref), unfold_ref_nii);
-unfold_info = niftiinfo(unfold_ref_nii);
-unfold_info.Transform.T = affine_unfold'; %note transposing here
-
-
-%convert inds from 0-(N-1) to phys inds in unfolded space, byapplying
-%affine (first cat into vec)
-unfold_coords_mat = cat(4,gu_,gv_,gw_);
-unfold_coords_mat = reshape(unfold_coords_mat,[prod(size(unfold_coords_mat,1,2,3)),3]);
-unfold_coords_mat = [unfold_coords_mat,ones(size(unfold_coords_mat,1),1)]';
-
-
-unfold_coords_phys = affine_unfold*unfold_coords_mat;
-unfold_coords_phys = unfold_coords_phys(1:3,:)';
-
-%reshape back to an image
-unfold_coords_phys_img = reshape(unfold_coords_phys,size(unfold_ref));
-
 
 for surf_i = 1:length(surf_names)
     io_ind = io_inds(surf_i);
@@ -102,8 +73,7 @@ for surf_i = 1:length(surf_names)
     
     %get surface
     unfold_midsurf_phys_img = squeeze(unfold_coords_phys_img(2:(end-1),2:(end-1),io_ind,:));
-    
-    unfold_midsurf_phys_vec = reshape(unfold_midsurf_phys_img,prod(size(unfold_midsurf_phys_img,1,2)),3);
+    unfold_midsurf_phys_vec = reshape(unfold_midsurf_phys_img,prod(n_steps_unfold_crop),3);
     
     
     %save as gifti
@@ -112,8 +82,8 @@ for surf_i = 1:length(surf_names)
     g.mat = eye(4,4);
     g.vertices = single(unfold_midsurf_phys_vec);
     
-    gii_unfold = sprintf('%s/%s.unfolded.surf.gii',out_folder,surf_name);
-    vtk_unfold = sprintf('%s/%s.unfolded.surf.vtk',out_folder,surf_name);
+    gii_unfold = sprintf('%s/%s.unfoldedtemplate.surf.gii',out_folder,surf_name);
+    vtk_unfold = sprintf('%s/%s.unfoldedtemplate.surf.vtk',out_folder,surf_name);
 
     save(g,gii_unfold,'Base64Binary');
     saveas(g,vtk_unfold);
