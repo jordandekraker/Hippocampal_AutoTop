bffeebeddae685ec8edc9af6d0ae8ad93147c1a0
akhanf <alik@robarts.ca>
2020-12-11 12:57:39 -0500

    removed hardcoding of input in laplacecoords

--- tools/AutoTops_TransformAndRollOut.m
+++ tools/AutoTops_TransformAndRollOut.m
@@ -45,9 +45,10 @@ end
 postprocess_labelmap(inlbl,outdir);
 
 % apply/refine Laplacian coordinate framework
-labelmap_LaplaceCoords(outdir);
+inlbl = [outdir 'labelmap-postProcess.nii.gz'];
+labelmap_LaplaceCoords(inlbl,outdir);
 
-% extract hippocampal midsurface and features
+% extract hippocampal midsurface and features - to be deprecated by gifti surfs
 coords_SurfMap(outdir);
 
 %create warps and giftis
